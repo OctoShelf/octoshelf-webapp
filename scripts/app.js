f533383b366da151623a7d7bfcb2438228cc7e85
Tyler Stark <theirondeveloper@gmail.com>
2016-05-27 00:35:23 -0700

    Basic fetch repo functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,79 @@
 
-console.log('ONE DOLLAR AND EIGHTY-SEVEN CENTS.');
+let repoSection = document.getElementById('repoSection');
+let addRepoForm = document.getElementById('addRepoForm');
+let addRepoInput = document.getElementById('addRepoInput');
 
-var addRepo = document.getElementById('addRepo');
-var addUser = document.getElementById('addUser');
 
-var addRepoInput = document.getElementById('addRepoInput');
-var addUserInput = document.getElementById('addUserInput');
+addRepoForm.addEventListener('submit', function(event) {
 
-addRepo.addEventListener('submit', function(event) {
   event.preventDefault();
+  addRepo(addRepoInput.value);
   addRepoInput.value = '';
 });
 
-addUser.addEventListener('submit', function(event) {
-  event.preventDefault();
-  addUserInput.value = '';
-});
+
+// TODO: Add everything below this to a WebWorker
+let repositories = [];
+let repositoriesSet = new Set();
+
+let repository = {
+  url: '',
+  fetchedDetails: false
+};
+
+
+function addRepo(url) {
+  if (repositoriesSet.has(url)) {
+    return;
+  }
+
+  let newRepository = Object.assign({}, repository, {
+    prs: [],
+    issues: [],
+    url: url
+  });
+
+  repositories.push(newRepository);
+  repositoriesSet.add(url);
+
+  getRepoDetails(newRepository);
+  drawEverything();
+}
+
+function getRepoDetails(repository) {
+
+  let { url, fetchedDetails } = repository;
+
+  if (fetchedDetails) {
+    return drawEverything();
+  }
+
+  // TODO: Update this to allow for corp github instances
+  let apiUrl = url.replace('https://github.com/', '');
+
+  fetch(`https://api.github.com/repos/${apiUrl}`)
+    .then(response => response.json())
+    .then(response => {
+      repository.name = response.name;
+      repository.fetchedDetails = true;
+    })
+    .then(drawEverything);
+}
+
+
+function drawEverything () {
+
+  let fragment = document.createDocumentFragment(),
+    article,
+    header;
+
+  repositories.forEach(function({ name, url }) {
+    article = document.createElement('article');
+    header = document.createTextNode(name || url);
+    article.appendChild(header);
+    fragment.appendChild(article);
+  });
+
+  repoSection.innerHTML = '';
+  repoSection.appendChild(fragment);
+}
