60580c9324326711d6e3f2bf86731e44357048e2
Tyler Stark <theirondeveloper@gmail.com>
2016-06-01 09:26:52 -0700

    Heroku binds to its own port, whups..

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.get('/', function (req, res) {
   res.render('index.ejs', githubApi);
 });
 
-app.listen(3000, function () {
-  console.log('OctoShelf, http://localhost:3000/');
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`OctoShelf, http://localhost:${port}/`);
 });
